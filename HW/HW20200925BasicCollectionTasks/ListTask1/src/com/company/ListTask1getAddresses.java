6fcf6b814823bb20f0fdfa6410431eb022718ce2
KomolaBZR <komolabenzinger@gmail.com>
2020-10-13 19:45:54 +0200

    add new Projects

---
+++ HW/HW20200925BasicCollectionTasks/ListTask1/src/com/company/ListTask1getAddresses.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListTask1getAddresses {
+
+    //Есть два класса: Address с полями улица и номер дома и Person с полями имя и Address.
+    //Нужно написать функцию: List<Address> getAddresses(List<Person> persons)
+
+    public static void main(String[] args) {
+
+        List<Person> persons = Arrays.asList(
+                new Person("Ivan", new Address("Friedrich str.", "7A")),
+                new Person("Maria", new Address("Mueller str.", "5")),
+                new Person("Stephan", new Address("Luxemburg str.", "8")),
+                new Person("John", new Address("Karl-Marx-Straße", "10")),
+                new Person("Amalia", new Address("Seestraße", "5")));
+
+        System.out.println("---------Actual List with Names and Addresses---------");
+        for (Person p : persons) {
+            System.out.println(p);
+        }
+        System.out.println("-------------------Get Addresses----------------------");
+        System.out.println(getAddresses(persons));
+
+
+
+    }
+
+    public static List<Address> getAddresses(List<Person> persons) {
+        List<Address> list = new ArrayList<>();
+        if (persons != null) {
+            for (Person p: persons) {
+                list.add(p.getAddress());
+            }
+            return list;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ HW/HW20200925BasicCollectionTasks/ListTask2/src/com/company/Task2ReturnListOfNamesWithoutNameswith4Letters.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Task2ReturnListOfNamesWithoutNameswith4Letters {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
